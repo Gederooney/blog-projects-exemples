6c2fecc064f8bf1ca701ec77620ade935fb263fe
gederooney <gyebadokpo61@gmail.com>
2023-11-13 10:03:40 -0500

    tailwinf components

---
+++ tailwindcss/text.js
@@ -0,0 +1,14 @@
+do {
+  eat();
+  sleep();
+  code();
+} while (notBurntOut());
+
+
+console.log("depression is real");
+
+
+if ("No Woman")
+	console.log("No Coding");
+else
+	console.log("Equality in Coding");
